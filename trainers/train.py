9873e998102da64129fbfee90fb523a1bce908c3
Michał Czuba <michal.czuba.1995@gmail.com>
2024-09-09 10:22:51 +0200

    comply to CR

--- trainers/train.py
+++ trainers/train.py
@@ -4,10 +4,10 @@ load model
 train model
 evaluate model
 """
-import network_diffusion as nd
-
+import argparse
 from dataclasses import dataclass
 
+import network_diffusion as nd
 from _data_set.nsl_data_utils.loaders.net_loader import load_network
 from _data_set.nsl_data_utils.loaders.sp_loader import get_gt_data
 from infmax_models.loader import load_model
@@ -21,7 +21,7 @@ class Network:
 
 # TODO: for now it's just a mock. we have to implement a real training pipeline
 
-def train(args):
+def train(args: argparse.Namespace) -> None:
 
     # load dataset
     networks = [Network(n, load_network(net_name=n, as_tensor=True)) for n in args["networks"]]
@@ -40,11 +40,25 @@ def train(args):
         print(f"Dataset: {net.name}")
 
         pred_seeds = model(network=net.graph)
-        pred_performance = evaluate_seed_set(net.graph, pred_seeds, proto, p, n_steps, n_repetitions)
+        pred_performance = evaluate_seed_set(
+            net=net.graph,
+            seed_set=pred_seeds,
+            protocol=proto,
+            probability=p,
+            n_steps=n_steps,
+            n_repetitions=n_repetitions,
+        )
         print(f"Predicted seed set: {pred_seeds}")
         print(f"{pred_performance.mean()}\n")
 
         ref_seeds = get_gt_data(net.name, proto, p, seed_size)
-        ref_performance = evaluate_seed_set(net.graph, ref_seeds, proto, p, n_steps, n_repetitions)
+        ref_performance = evaluate_seed_set(
+            net=net.graph,
+            seed_set=ref_seeds,
+            protocol=proto,
+            probability=p,
+            n_steps=n_steps,
+            n_repetitions=n_repetitions
+        )
         print(f"Reference seed set: {ref_seeds}")
         print(f"{ref_performance.mean()}\n")
