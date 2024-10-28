177422ba7f1841d40d4923b7b3ea2841416c3d74
mstolarski <stolarski.mateusz3@gmail.com>
2024-10-28 16:04:25 +0100

    chore: updated net loader

--- src/datamodule/loader.py
+++ src/datamodule/loader.py
@@ -1,15 +1,62 @@
-from typing import Any
-
-from torch_geometric.data.lightning import LightningDataset
-from torch_geometric.typing import EdgeType, NodeType
+from typing import Any, Literal
 
 from _data_set.nsl_data_utils.loaders.net_loader import load_network
 from _data_set.nsl_data_utils.loaders.sp_loader import load_sp
+from sklearn.model_selection import train_test_split
 from src import MODULE_PATH
 from src.dataset.base_hetero_dataset import BaseHeteroDataset
 from src.dataset.data_frame_hetero_dataset import DataFrameHeteroDataset
 from src.utils.multilayer_network import MultilayerNetworkInfo
 from src.utils.worker import get_num_workers
+from torch_geometric.data.lightning import LightningDataset
+from torch_geometric.typing import EdgeType, NodeType
+
+
+def _load_networks_info(
+    networks_config: list[dict[str, Any]],
+    label: str,
+    protocol: str,
+    random_seed: int,
+    dataset_type: Literal["train", "val", "test"],
+) -> list[MultilayerNetworkInfo]:
+    networks_info = []
+    for network_config in networks_config:
+        network_name = network_config["name"]
+        network = load_network(
+            net_name=network_name,
+            as_tensor=False,
+        )
+        spreading_potential = load_sp(network_name)
+
+        network_info = [
+            MultilayerNetworkInfo(
+                network=mln,
+                output_label_name=label,
+                spreading_potential=spreading_potential[name],
+                network_name=f"{network_name}_{name}",
+                protocol=protocol,
+                features_type=network_config["features_type"],
+            )
+            for name, mln in network.items()
+        ]
+
+        if network_config["val_split"]:
+            train_networks, val_networks = train_test_split(
+                network_info,
+                test_size=0.2,
+                random_state=random_seed,
+            )
+            match dataset_type:
+                case "train":
+                    networks_info.extend(train_networks)
+                case "val":
+                    networks_info.extend(val_networks)
+                case "test":
+                    raise ValueError(f"Invalid option: val_split for test network")
+        else:
+            networks_info.extend(network_info)
+
+    return networks_info
 
 
 def _get_dataset(
@@ -19,25 +66,20 @@ def _get_dataset(
     input_dim: int,
     output_dim: int,
     protocol: str,
+    random_seed: int,
+    dataset_type: Literal["train", "val", "test"],
 ) -> BaseHeteroDataset:
     match data_name:
         case DataFrameHeteroDataset.__name__:
             return DataFrameHeteroDataset(
                 root=str(MODULE_PATH.parent / "data"),
-                networks=[
-                    MultilayerNetworkInfo(
-                        network=load_network(
-                            net_name=network_config["name"],
-                            as_tensor=False,
-                        ),
-                        output_label_name=label,
-                        spreading_potential=load_sp(network_config["name"]),
-                        network_name=network_config["name"],
-                        protocol=protocol,
-                        features_type=network_config["features_type"],
-                    )
-                    for network_config in networks_config
-                ],
+                networks=_load_networks_info(
+                    networks_config=networks_config,
+                    label=label,
+                    protocol=protocol,
+                    random_seed=random_seed,
+                    dataset_type=dataset_type,
+                ),
                 input_dim=input_dim,
                 output_dim=output_dim,
             )
@@ -54,6 +96,8 @@ def get_datasets(config: dict[str, Any]) -> dict[str, BaseHeteroDataset]:
         input_dim=config["model"]["parameters"]["input_dim"],
         output_dim=config["model"]["parameters"]["output_dim"],
         protocol=config["data"]["protocol"],
+        random_seed=config["base"]["random_seed"],
+        dataset_type="train",
     )
     val_dataset = _get_dataset(
         data_name=config["data"]["name"],
@@ -62,6 +106,8 @@ def get_datasets(config: dict[str, Any]) -> dict[str, BaseHeteroDataset]:
         input_dim=config["model"]["parameters"]["input_dim"],
         output_dim=config["model"]["parameters"]["output_dim"],
         protocol=config["data"]["protocol"],
+        random_seed=config["base"]["random_seed"],
+        dataset_type="val",
     )
     test_dataset = _get_dataset(
         data_name=config["data"]["name"],
@@ -70,6 +116,8 @@ def get_datasets(config: dict[str, Any]) -> dict[str, BaseHeteroDataset]:
         input_dim=config["model"]["parameters"]["input_dim"],
         output_dim=config["model"]["parameters"]["output_dim"],
         protocol=config["data"]["protocol"],
+        random_seed=config["base"]["random_seed"],
+        dataset_type="test",
     )
 
     return {
