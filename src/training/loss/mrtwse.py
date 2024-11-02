8081a2b21cfc9f50466ab49b03c5198013642988
Michał Czuba <michal.czuba.1995@gmail.com>
2024-11-02 11:11:43 +0000

    added custom loss mrtwse

---
+++ src/training/loss/mrtwse.py
@@ -0,0 +1,41 @@
+"""Implementation of the cusrom Mean Root Total Weighted Squared Error."""
+
+import torch
+
+
+class MRTWSE(torch.nn.Module):
+    def __init__(
+        self,
+        w_sl: float,
+        w_e: float,
+        w_pin: float,
+        w_pit: float,
+    ) -> None:
+        """
+        Mean Root Total Weighted Squared Error.
+
+        1. scale weights by softmax so that they sum to 1
+        2. compute squared error
+        3. scale it by weights
+        4. for each actor sum the vector of partial errors and obtain squared root of it
+        5. return mean value of such errors along the batch
+
+        :param w_sl: weight for simulation length
+        :param w_e: weight for exposed number
+        :param w_pin: weight for peak infection
+        :param w_pit: weight for peak iteration
+        """
+        super().__init__()
+        raw_weights = torch.tensor([w_sl, w_e, w_pin, w_pit], dtype=torch.float32)
+        self.weights = torch.softmax(raw_weights, dim=0)
+        self._bypass_flag = False  # TODO: also it seems that Lightning ignores device of the loss
+
+    def forward(self, y_hat: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        if not self._bypass_flag:
+            self.weights = self.weights.to(device=y.device)
+            self._bypass_flag = True
+        se = (y_hat - y) ** 2  # squared error
+        wse = se * self.weights.expand_as(se)  # weighted se
+        rtwse = torch.sqrt(wse.sum(dim=1))  # root total wse (for each y_i sum err and sqrt them)
+        mrtwse = rtwse.mean()  # compute mean rtwse in the batch
+        return mrtwse
--- src/utils/wrapper.py
+++ src/utils/wrapper.py
@@ -4,6 +4,7 @@ from torch.nn import CrossEntropyLoss, MSELoss
 from torch.nn.modules.loss import _Loss
 
 from src.training.loss.weighted_mse import WeightedMSE
+from src.training.loss.mrtwse import MRTWSE
 
 
 def get_loss(
@@ -19,6 +20,9 @@ def get_loss(
 
         case WeightedMSE.__name__:
             return WeightedMSE(**loss_args)
+
+        case MRTWSE.__name__:
+            return MRTWSE(**loss_args)
 
         case _:
             raise AttributeError(f"Unknown loss function: {loss_name}")
