6ffd45f4b55bd21c883924bc8e1be9ae407fe818
mateuszStolarski <79900049+mateuszStolarski@users.noreply.github.com>
2024-12-18 11:08:16 +0100

    fix: handle -1 worker number

--- src/utils/worker.py
+++ src/utils/worker.py
@@ -4,4 +4,4 @@ from typing import Any
 
 def get_num_workers(config: dict[str, Any]) -> int:
     num_workers = config["data"]["num_workers"]
-    return num_workers if num_workers != -1 else cpu_count() - 1
+    return num_workers if num_workers != -1 else (cpu_count() - 1) // 4
